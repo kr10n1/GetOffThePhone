43037dc11ab2696a02df3cbb8c030bfffe8a7c15
Adam Slomka <adam.slomka@ipfdigital.com>
2019-08-17 19:26:24 +0200

    added icon content but it does not help. changed sdk to 28 to allow documentation download

--- app/src/main/java/com/example/getoffthephone/MainActivity.java
+++ app/src/main/java/com/example/getoffthephone/MainActivity.java
@@ -1,24 +1,32 @@
 package com.example.getoffthephone;
 
-import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 
-
-
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationCompat.Action;
+import androidx.core.app.NotificationCompat.BigTextStyle;
 import androidx.core.content.ContextCompat;
+
+import static android.app.Notification.DEFAULT_SOUND;
+import static android.app.NotificationManager.IMPORTANCE_HIGH;
+import static android.app.PendingIntent.FLAG_UPDATE_CURRENT;
+import static android.content.Context.NOTIFICATION_SERVICE;
+import static com.example.getoffthephone.R.drawable.ic_drink_notification;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String CHANNELID = "channelid";
     public static final int ID = 9999;
+    public static final int REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,25 +40,41 @@ public class MainActivity extends AppCompatActivity {
     public void notify(View view) {
 
         NotificationManager notificationManager = (NotificationManager)
-                this.getSystemService(Context.NOTIFICATION_SERVICE);
+                this.getSystemService(NOTIFICATION_SERVICE);
 
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNELID)
                 .setColor(ContextCompat.getColor(this, R.color.colorPrimary))
-                .setSmallIcon(R.drawable.ic_drink_notification)
+                .setSmallIcon(ic_drink_notification)
 //                .setLargeIcon(largeIcon(context))
                 .setContentTitle(this.getString(R.string.charging_reminder_notification_title))
                 .setContentText(this.getString(R.string.charging_reminder_notification_body))
-                .setStyle(new NotificationCompat.BigTextStyle().bigText(this.getString(R.string.charging_reminder_notification_body)))
-                .setDefaults(Notification.DEFAULT_VIBRATE)
+                .setStyle(new BigTextStyle().bigText(this.getString(R.string.charging_reminder_notification_body)))
+                .setDefaults(DEFAULT_SOUND)
                 .setContentText("blablabla")
+                .setContentIntent(getPendingIntent())
+                .addAction(action())
                 .setAutoCancel(true);
 
 
         NotificationChannel mChannel = new NotificationChannel(
-                "",
+                "channel",
                 this.getString(R.string.main_notification_channel_name),
-                NotificationManager.IMPORTANCE_HIGH);
+                IMPORTANCE_HIGH);
         notificationManager.createNotificationChannel(mChannel);
         notificationManager.notify(ID, builder.build());
+    }
+
+    private Action action() {
+        return new Action(ic_drink_notification, "test", getPendingIntent());
+    }
+
+    private PendingIntent getPendingIntent() {
+        return PendingIntent.getService(this, REQUEST_CODE, intent(), FLAG_UPDATE_CURRENT);
+    }
+
+    private Intent intent() {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setAction("action!");
+        return intent;
     }
 }
